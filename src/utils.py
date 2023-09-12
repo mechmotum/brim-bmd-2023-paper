6dec49db409b22fbb80fcbc39aa6f072c544b715
TJStienstra <timostienstra00@gmail.com>
2023-09-12 14:51:22 +0200

    Add enum parser

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import argparse
+import enum
 import json
 from copy import copy
 
@@ -14,6 +16,35 @@ from scipy.interpolate import CubicSpline
 from symmeplot import PlotBody
 
 from container import DataStorage
+
+
+class EnumAction(argparse.Action):
+    """Argparse action for handling Enums.
+
+    Source: https://stackoverflow.com/a/60750535/20185124
+    """
+
+    def __init__(self, **kwargs):
+        # Pop off the type value
+        enum_type = kwargs.pop("type", None)
+
+        # Ensure an Enum subclass is provided
+        if enum_type is None:
+            raise ValueError("type must be assigned an Enum when using EnumAction")
+        if not issubclass(enum_type, enum.Enum):
+            raise TypeError("type must be an Enum when using EnumAction")
+
+        # Generate choices from the Enum
+        kwargs.setdefault("choices", tuple(e.name for e in enum_type))
+
+        super(EnumAction, self).__init__(**kwargs)
+
+        self._enum = enum_type
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        # Convert value back into an Enum
+        value = self._enum[values]
+        setattr(namespace, self.dest, value)
 
 
 class NumpyEncoder(json.JSONEncoder):
